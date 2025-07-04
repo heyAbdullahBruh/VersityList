5bf0138bb649c9ec5b1fc6f12d6d423ec8b969e9
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-04 10:34:39 +0600

    App start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import styles from "./App.module.css";
+
+
+function App() {
+ 
+  return (
+    <>
+      <main className={styles.app}>
+        <h1>US Universities</h1>
+        <h1>Hello US versity</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
