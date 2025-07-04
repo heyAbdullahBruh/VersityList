5f4f2abcc8ac7d98232baac4d8d0ddcd63fee4f6
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-04 10:38:04 +0600

    #updae1.1 loading created

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,29 @@
+
+const Loading = () => {
+  return (
+    <>
+      <span className="loader"></span>
+      <style jsx="true">{`
+        .loader {
+          width: 15px;
+          height: 15px;
+          border: 2px solid transparent;
+          border-top: 2px solid white;
+          border-radius: 50%;
+          animation: spin 0.8s linear infinite;
+          display: inline-block;
+        }
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default Loading;
