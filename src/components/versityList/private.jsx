f85d15a4212fed87a16c3328dc8c262248a5bfa8
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-04 10:36:06 +0600

    Private And Public created

---
+++ src/components/versityList/private.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import Loading from "../loading/Loading";
+import styles from "./versity.module.css";
+
+const PrivateUni = () => {
+  const [versities, setVersities] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchPrivateVerstyData = async () => {
+    setLoading(true);
+    try {
+      const respons = await fetch("/data/privateUni.json");
+      const data = await respons.json();
+      setVersities(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPrivateVerstyData();
+  }, []);
+
+  return (
+    <section className={styles.versity}>
+      <h1 id={styles.headV}>Private University</h1>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          {versities?.map((uni) => {
+            const { id, name, type, rank, location } = uni;
+            return (
+              <article key={id}>
+                <h2>{name}</h2>
+                <p>{type}</p>
+                <h3>
+                  QS Rank {"->"} {rank}
+                </h3>
+                <h4>
+                  Location{"-->"} {location}
+                </h4>
+              </article>
+            );
+          })}
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default PrivateUni;
---
+++ src/components/versityList/public.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PublicUni = () => {
+    return (
+        <div>
+            <h1>Public University</h1>
+        </div>
+    );
+};
+
+export default PublicUni;
