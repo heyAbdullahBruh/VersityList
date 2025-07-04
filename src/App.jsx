36e4708b9d4ac805359867dd1d9b3841f8ab04e6
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-04 10:41:19 +0600

    #updae1.2 app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-
+import { useState } from "react";
 import styles from "./App.module.css";
-
+import PublicUni from "./components/versityList/public";
+import PrivateUni from "./components/versityList/private";
 
 function App() {
- 
+  const [isPublic, setIsPublic] = useState(false);
   return (
     <>
       <main className={styles.app}>
         <h1>US Universities</h1>
-        <h1>Hello US versity</h1>
+        <aside className={styles.controller}>
+          <button
+            className={isPublic && `${styles.active}`}
+            onClick={() => setIsPublic(true)}
+          >
+            Public Uni
+          </button>
+
+          <button
+            className={isPublic || `${styles.active}`}
+            onClick={() => setIsPublic(false)}
+          >
+            Private Uni
+          </button>
+        </aside>
+
+        {isPublic ? (
+          <aside id={styles.publicUni}>
+            <PublicUni />
+          </aside>
+        ) : (
+          <aside id={styles.privateUni}>
+            <PrivateUni />
+          </aside>
+        )}
       </main>
     </>
   );
