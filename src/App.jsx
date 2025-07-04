7dec84fa28975d26ef997f65e83e9cb14a00d805
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-04 21:02:55 +0600

    #updae201 footer added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ function App() {
   return (
     <>
       <Routers />
+      
+      <hr /> 
+
+      <footer style={{textAlign:'center'}}>
+        <h3 style={{fontFamily:'monospace'}}>&copy;Copyright All Right Reserve || Made By <a href="https://pietenium.vercel.app" target="_blank">Pietenium</a></h3>
+        <h4>Developer — <a href="https://www.linkedin.com/in/heyabdullahbro" target="_blank">Abdullah Shayed</a></h4>
+      </footer>
     </>
   );
 }
